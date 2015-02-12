390defc4556ec402f88313464d65995a0ebc0d4b
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2015-02-12 15:50:51 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""Installation script."""
+from os import path
+from setuptools import find_packages, setup
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, 'README.md')) as f:
+    LONG_DESCRIPTION = f.read().strip()
+
+setup(
+    name='fuel',
+    version='0.1a1',  # PEP 440 compliant
+    description='Data pipeline framework for machine learning',
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/bartvm/fuel.git',
+    author='Universite de Montreal',
+    license='MIT',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='dataset data iteration pipeline processing',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['six'])
