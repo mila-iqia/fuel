1daa8ab1f8a8f1e012ffa752bf35d08549f83666
dima <serdyuk.dmitriy@gmail.com>
2015-02-24 15:14:33 -0500

    Added test for utils

---
+++ tests/test_utils.py
@@ -0,0 +1,25 @@
+import pickle
+from six.moves import range
+
+from fuel.utils import do_not_pickle_attributes
+
+
+@do_not_pickle_attributes("non_pickable", "bulky_attr")
+class TestClass(object):
+    def __init__(self):
+        self.load()
+
+    def load(self):
+        self.bulky_attr = list(range(100))
+        self.non_pickable = open(__file__, 'r')
+
+
+def test_do_not_pickle_attributes():
+    cl = TestClass()
+
+    dump = pickle.dumps(cl)
+
+    loaded = pickle.loads(dump)
+    assert loaded.bulky_attr == list(range(100))
+    assert loaded.non_pickable is not None
+
