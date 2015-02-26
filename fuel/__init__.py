b598a8c9c71b78082a7d0c65cd6a724e0116bd91
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-02-25 21:11:53 -0500

    Add versioning information

--- fuel/__init__.py
+++ fuel/__init__.py
@@ -1,1 +1,15 @@
+import os.path
+from pkg_resources import get_distribution, DistributionNotFound
+
 from fuel.config_parser import config  # flake8: noqa
+
+try:
+    DIST = get_distribution('fuel')
+    DIST_LOC = os.path.normcase(DIST.location)
+    HERE = os.path.normcase(__file__)
+    if not HERE.startswith(os.path.join(DIST_LOC, 'fuel')):
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = 'not installed'
+else:
+    __version__ = DIST.version
