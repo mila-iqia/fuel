30434bd52fc2982b72db04fa1ada14c6464ac37f
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-03-28 18:42:13 -0400

    Add PyTables to setup.py

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     ],
     keywords='dataset data iteration pipeline processing',
     packages=find_packages(exclude=['tests']),
-    install_requires=['six', 'picklable_itertools', 'toolz', 'pyyaml', 'h5py'],
+    install_requires=['six', 'picklable_itertools', 'toolz', 'pyyaml', 'h5py',
+                      'tables'],
     scripts=['bin/fuel-convert']
 )
