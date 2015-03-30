2c758656220419082481820d2fb18974263ba5af
Vincent Dumoulin <vi.dumoulin@gmail.com>
2015-03-30 10:25:33 -0400

    Refactor binarized MNIST tests

--- tests/test_binarized_mnist.py
+++ tests/test_binarized_mnist.py
@@ -1,10 +1,13 @@
+import os
+
 from numpy.testing import assert_raises
 
+from fuel import config
 from fuel.datasets import BinarizedMNIST
 from tests import skip_if_not_available
 
 
-def test_binarized_mnist():
+def test_binarized_mnist_train():
     skip_if_not_available(datasets=['binarized_mnist'])
 
     mnist_train = BinarizedMNIST('train')
@@ -14,12 +17,20 @@ def test_binarized_mnist():
     assert mnist_train.num_examples == 50000
     mnist_train.close(handle)
 
+
+def test_binarized_mnist_valid():
+    skip_if_not_available(datasets=['binarized_mnist'])
+
     mnist_valid = BinarizedMNIST('valid')
     handle = mnist_valid.open()
     data = mnist_valid.get_data(handle, slice(0, 10000))[0]
     assert data.shape == (10000, 1, 28, 28)
     assert mnist_valid.num_examples == 10000
     mnist_valid.close(handle)
+
+
+def test_binarized_mnist_test():
+    skip_if_not_available(datasets=['binarized_mnist'])
 
     mnist_test = BinarizedMNIST('test')
     handle = mnist_test.open()
@@ -28,11 +39,11 @@ def test_binarized_mnist():
     assert mnist_test.num_examples == 10000
     mnist_test.close(handle)
 
+
+def test_binarized_mnist_invalid_split():
     assert_raises(ValueError, BinarizedMNIST, 'dummy')
 
-    mnist_test_flattened = BinarizedMNIST(
-        'test', flatten=['features'], load_in_memory=True)
-    handle = mnist_test_flattened.open()
-    data = mnist_test_flattened.get_data(handle, slice(0, 10000))[0]
-    assert data.shape == (10000, 784)
-    mnist_test_flattened.close(handle)
+
+def test_binarized_mnist_data_path():
+    assert BinarizedMNIST('train').data_path == os.path.join(
+        config.data_path, 'binarized_mnist.hdf5')
