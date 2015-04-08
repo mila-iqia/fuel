16ffcb30d980200a03d372aff12c4801f56d7a12
Jorg Bornschein <jb@capsec.org>
2015-04-08 11:24:43 -0400

    Add support for multiple classes to toy.Spiral

--- fuel/datasets/toy.py
+++ fuel/datasets/toy.py
@@ -16,10 +16,10 @@ class Spiral(IndexableDataset):
 
         from fuel.datasets.toy import Spiral
 
-        ds = Spiral()
-        features, position = ds.get_data(None, slice(0, 100))
+        ds = Spiral(classes=3)
+        features, position, label = ds.get_data(None, slice(0, 500))
 
-        plt.title("Datapoints drawn from Spiral(classes=2)")
+        plt.title("Datapoints drawn from Spiral(classes=3)")
         plt.scatter(features[:,0], features[:,1], c=position)
         plt.xlim(-1.2, 1.2)
         plt.ylim(-1.2, 1.2)
@@ -32,19 +32,23 @@ class Spiral(IndexableDataset):
     cycles : float
     sd : float
     """
-    def __init__(self, n_datapoints=1000, cycles=1., sd=0.0, **kwargs):
+    def __init__(self, n_datapoints=1000, classes=1, cycles=1., sd=0.0, **kwargs):
         # Create dataset
         pos = numpy.random.uniform(size=(n_datapoints,), low=0, high=cycles)
+        label = numpy.random.randint(size=(n_datapoints,), low=0, high=classes)
+
         radius = (2*pos+1) / 3.
+        phase_offset = label * (2*numpy.pi) / classes
         
         features = numpy.zeros(shape=(n_datapoints, 2), dtype='float32')
 
-        features[:,0] = radius * numpy.sin(2*numpy.pi*pos)
-        features[:,1] = radius * numpy.cos(2*numpy.pi*pos)
+        features[:,0] = radius * numpy.sin(2*numpy.pi*pos + phase_offset)
+        features[:,1] = radius * numpy.cos(2*numpy.pi*pos + phase_offset)
 
         data = OrderedDict([
             ('features', features),
             ('position', pos),
+            ('label', label),
         ])
 
         super(Spiral, self).__init__(data, **kwargs)
