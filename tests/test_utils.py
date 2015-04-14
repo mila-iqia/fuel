b82dbd63aedf8a6a6af494b6d6be697a9f4230d5
Vincent Dumoulin <vi.dumoulin@gmail.com>
2015-04-14 15:04:23 -0400

    Add unit test for expand_axis_label

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 import pickle
 from six.moves import range
 
-from fuel.utils import do_not_pickle_attributes
+from fuel.utils import do_not_pickle_attributes, expand_axis_label
 
 
 @do_not_pickle_attributes("non_pickable", "bulky_attr")
@@ -22,3 +22,13 @@ def test_do_not_pickle_attributes():
     loaded = pickle.loads(dump)
     assert loaded.bulky_attr == list(range(100))
     assert loaded.non_pickable is not None
+
+
+def test_expand_axis_label():
+    assert expand_axis_label('b') == 'batch'
+    assert expand_axis_label('c') == 'channel'
+    assert expand_axis_label('t') == 'time'
+    assert expand_axis_label('0') == 'axis_0'
+    assert expand_axis_label('1') == 'axis_1'
+    assert expand_axis_label('0b') == '0b'
+    assert expand_axis_label('') == ''
