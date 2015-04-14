7dae777a69337aa3eb0ae2075a664e2a9b56522b
Vincent Dumoulin <vi.dumoulin@gmail.com>
2015-04-13 20:25:17 -0400

    Fix failing Scrutinizer test

--- fuel/converters/__init__.py
+++ fuel/converters/__init__.py
@@ -5,9 +5,9 @@ their corresponding built-in dataset.
 
 Conversion functions accept a single argument, `subparser`, which is an
 `argparse.ArgumentParser` instance that it needs to fill with its own
-specific arguments. They should set a `func` default argument for the subparser
-with a function that will get called and given the parsed command-line
-arguments, and is expected to download the required files.
+specific arguments. They should set a `func` default argument for the
+subparser with a function that will get called and given the parsed
+command-line arguments, and is expected to download the required files.
 
 """
 from fuel.converters.binarized_mnist import binarized_mnist
