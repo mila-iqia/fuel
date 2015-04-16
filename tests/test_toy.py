a2c7a75f8bc9493448ad753e2614f05fce99641d
Jorg Bornschein <jb@capsec.org>
2015-04-16 18:12:34 -0400

    Tests for toy datasets

--- tests/test_toy.py
+++ tests/test_toy.py
@@ -1,5 +1,5 @@
 
-from fuel.datasets.toy import Spiral
+from fuel.datasets.toy import Spiral, SwissRoll
 
 
 def test_spiral():
@@ -7,10 +7,32 @@ def test_spiral():
 
     features, position, label = ds.get_data(None, slice(0, 1000))
 
+    assert features.ndim == 2
     assert features.shape[0] == 1000
+    assert features.shape[1] == 2
+
+    assert position.ndim == 1
     assert position.shape[0] == 1000
+
+    assert label.ndim == 1
     assert label.shape[0] == 1000
 
     assert features.max() <= 1.
     assert position.max() <= 1.
-    assert label.max() == 2
+    assert label.max() == 1
+
+def test_swiossroll():
+    ds = SwissRoll(num_examples=1000)
+
+    features, position = ds.get_data(None, slice(0, 1000))
+
+    assert features.ndim == 2
+    assert features.shape[0] == 1000
+    assert features.shape[1] == 3
+
+    assert position.ndim == 2
+    assert position.shape[0] == 1000
+    assert position.shape[1] == 2
+
+    assert features.max() <= 1.
+    assert position.max() <= 1.
