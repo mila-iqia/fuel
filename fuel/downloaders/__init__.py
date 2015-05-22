9ab2d6238d864c6a393a2d09d05f5452a1498c59
Vincent Dumoulin <vi.dumoulin@gmail.com>
2015-05-22 13:38:45 -0400

    Add downloader for SVHN (format 1 *and* 2)

--- fuel/downloaders/__init__.py
+++ fuel/downloaders/__init__.py
@@ -10,9 +10,11 @@ from fuel.downloaders import binarized_mnist
 from fuel.downloaders import cifar10
 from fuel.downloaders import cifar100
 from fuel.downloaders import mnist
+from fuel.downloaders import svhn
 
 all_downloaders = (
     ('binarized_mnist', binarized_mnist.fill_subparser),
     ('cifar10', cifar10.fill_subparser),
     ('cifar100', cifar100.fill_subparser),
     ('mnist', mnist.fill_subparser))
+    ('svhn', svhn.fill_subparser))
---
+++ fuel/downloaders/svhn.py
@@ -0,0 +1,31 @@
+from fuel.downloaders.base import default_downloader
+
+
+def svhn_downloader(which_format, directory, clear=False):
+    suffix = {1: '.tar.gz', 2: '_32x32.mat'}[which_format]
+    sets = ['train', 'test', 'extra']
+    default_downloader(
+        directory=directory,
+        urls=[None for f in sets],
+        filenames=['{}{}'.format(s, suffix) for s in sets],
+        url_prefix='http://ufldl.stanford.edu/housenumbers/',
+        clear=clear)
+
+
+def fill_subparser(subparser):
+    """Sets up a subparser to download the SVHN dataset files.
+
+    The SVHN dataset files (`{train,test,extra}{.tar.gz,_32x32.mat}`)
+    are downloaded from the official website [SVHN].
+
+    .. [SVHN] http://ufldl.stanford.edu/housenumbers/
+
+    Parameters
+    ----------
+    subparser : :class:`argparse.ArgumentParser`
+        Subparser handling the `svhn` command.
+
+    """
+    subparser.add_argument(
+        "which_format", help="which dataset format", type=int, choices=(1, 2))
+    subparser.set_defaults(func=svhn_downloader)
