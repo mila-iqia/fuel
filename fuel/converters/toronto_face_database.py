df1cc01c4682026adfa4694421ba27f99130e50e
Jorg Bornschein <jb@capsec.org>
2015-05-25 18:51:48 -0400

    Make flake8 happy

--- fuel/converters/toronto_face_database.py
+++ fuel/converters/toronto_face_database.py
@@ -1,13 +1,13 @@
 import os
 
 import h5py
-import numpy
 
 from scipy.io import loadmat
 
 from fuel.converters.base import fill_hdf5_file, check_exists
 
 ALL_FILES = ['TFD_48x48.mat']
+
 
 @check_exists(required_files=ALL_FILES)
 def convert_toronto_face_database(input_directory, output_fname):
@@ -26,30 +26,31 @@ def convert_toronto_face_database(input_directory, output_fname):
     with h5py.File(output_fname, mode="w") as h5file:
 
         tfd = loadmat(os.path.join(input_directory, 'TFD_48x48.mat'))
-        
+
         folds = tfd['folds']
         features = tfd['images'].reshape([-1, 1, 48, 48])
         expression_targets = tfd['labs_ex']
         identity_targets = tfd['labs_id']
-    
-        unlabeled_mask = folds[:,0] == 0
-        train1_mask = folds[:,0] == 1
-        valid1_mask = folds[:,0] == 2
-        test1_mask  = folds[:,0] == 3
-    
-        data = (('unlabeled', 'features', features[unlabeled_mask]),
-                ('train', 'features', features[train1_mask]),
-                ('train', 'expression_targets', expression_targets[train1_mask]),
-                ('train', 'identity_targets', identity_targets[train1_mask]),
-                ('valid', 'features', features[valid1_mask]),
-                ('valid', 'expression_targets', expression_targets[valid1_mask]),
-                ('valid', 'identity_targets', identity_targets[valid1_mask]),
-                ('test', 'features', features[test1_mask]),
-                ('test', 'expression_targets', expression_targets[test1_mask]),
-                ('test', 'identity_targets', identity_targets[test1_mask]),
+
+        unlabeled_mask = folds[:, 0] == 0
+        train1_mask = folds[:, 0] == 1
+        valid1_mask = folds[:, 0] == 2
+        test1_mask = folds[:, 0] == 3
+
+        data = (
+            ('unlabeled', 'features', features[unlabeled_mask]),
+            ('train', 'features', features[train1_mask]),
+            ('train', 'expression_targets', expression_targets[train1_mask]),
+            ('train', 'identity_targets', identity_targets[train1_mask]),
+            ('valid', 'features', features[valid1_mask]),
+            ('valid', 'expression_targets', expression_targets[valid1_mask]),
+            ('valid', 'identity_targets', identity_targets[valid1_mask]),
+            ('test', 'features', features[test1_mask]),
+            ('test', 'expression_targets', expression_targets[test1_mask]),
+            ('test', 'identity_targets', identity_targets[test1_mask]),
         )
         fill_hdf5_file(h5file, data)
-    
+
         for i, label in enumerate(('batch', 'channel', 'height', 'width')):
             h5file['features'].dims[i].label = label
 
