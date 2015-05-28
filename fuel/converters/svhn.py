641c8e6d2402a9d032c25701104b5f06d64ea671
Vincent Dumoulin <vi.dumoulin@gmail.com>
2015-05-28 09:12:48 -0400

    Fix (another) bug in bounding box shapes

--- fuel/converters/svhn.py
+++ fuel/converters/svhn.py
@@ -57,7 +57,7 @@ def convert_svhn_format_1(directory, output_file):
                              [(source, 'uint16') for source in sources[2:]])
         source_shape_labels = dict(
             [('features', ('channel', 'height', 'width')),
-             ('targets', 'index')] +
+             ('targets', ('index',))] +
             [(source, ('bounding_box',)) for source in sources[2:]])
 
         def extract_tar(split):
