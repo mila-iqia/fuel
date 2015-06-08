6f1109ba8ccca97413f8708ab1e82beeabb34000
Jorg Bornschein <jb@capsec.org>
2015-06-08 15:13:25 -0400

    Support both 48x48 and 96x96

--- fuel/converters/toronto_face_database.py
+++ fuel/converters/toronto_face_database.py
@@ -3,30 +3,42 @@ import h5py
 
 from scipy.io import loadmat
 
-from fuel.converters.base import fill_hdf5_file, check_exists
+from fuel.converters.base import fill_hdf5_file, MissingInputFiles
 
-ALL_FILES = ['TFD_48x48.mat']
+TFD_INPUT_FILE = 'TFD_{}x{}.mat'
 
 
-@check_exists(required_files=ALL_FILES)
-def convert_toronto_face_database(directory, output_file):
+def convert_tfd(size, directory, output_file):
     """Converts the Toronto Face Database to HDF5.
 
     ToDo
 
     Parameters
     ----------
+    size : int
+        Either 48 or 96. Indicates whether the dataset containing 48x48 sized
+        images should be converted, or the one containing 96x96 sized images.
     directory : str
         Directory in which the required input files reside.
     output_file : str
         Where to save the converted dataset.
 
     """
+    if size not in (48, 96):
+        ValueError("TFD size need to be 48 or 96")
+
+    input_file = TFD_INPUT_FILE.format(size, size)
+    input_file = os.path.join(directory, input_file)
+    if not os.path.isfile(input_file):
+        raise MissingInputFiles('Required files missing', [input_file])
+
+    output_file = output_file.format(size)
+
     with h5py.File(output_file, mode="w") as h5file:
-        tfd = loadmat(os.path.join(directory, 'TFD_48x48.mat'))
+        tfd = loadmat(input_file)
 
         folds = tfd['folds']
-        features = tfd['images'].reshape([-1, 1, 48, 48])
+        features = tfd['images'].reshape([-1, 1, size, size])
         expression_targets = tfd['labs_ex']
         identity_targets = tfd['labs_id']
 
@@ -68,4 +80,9 @@ def fill_subparser(subparser):
         Subparser handling the `toronto_face_database` command.
 
     """
-    subparser.set_defaults(func=convert_toronto_face_database)
+    subparser.add_argument(
+        "size", type=int, choices=(48, 96),
+        help="height/width of the datapoints")
+    subparser.set_defaults(
+        func=convert_tfd,
+        output_file=os.path.join(os.getcwd(), 'toronto_face_database{}.hdf5'))
--- fuel/datasets/toronto_face_database.py
+++ fuel/datasets/toronto_face_database.py
@@ -16,17 +16,16 @@ class TorontoFaceDatabase(H5PYDataset):
     In order to use this dataset you need to get the permissions/licenses
     from all sources:
 
-    ======== ================================================================ ===== ==================================  
-    Dataset  URL                                                              Price Notes
-    ======== ================================================================ ===== ==================================
-    JACFEE   http://www.humintell.com/for-use-in-research/                    $175  Get the Standard Expressor version
-    POFA     http://www.paulekman.com/product/pictures-of-facial-affect-pofa/ $175
-    KDEF     http://www.emotionlab.se/resources/kdef
-    CK+      http://www.pitt.edu/~emotion/ck-spread.htm
-    MMI      http://www.mmifacedb.com/
-    MSFDE    http://psychophysiolab.com/msfde/terms.php
-    NimStim  http://www.macbrain.org/resources.htm
-    RadBound http://www.socsci.ru.nl:8180/RaFD2/RaFD?p=register
+    * JACFEE   http://www.humintell.com/for-use-in-research/
+               ($175  Get the Standard Expressor version)
+    * POFA     http://www.paulekman.com/product/
+                pictures-of-facial-affect-pofa/ ($175)
+    * KDEF     http://www.emotionlab.se/resources/kdef
+    * CK+      http://www.pitt.edu/~emotion/ck-spread.htm
+    * MMI      http://www.mmifacedb.com/
+    * MSFDE    http://psychophysiolab.com/msfde/terms.php
+    * NimStim  http://www.macbrain.org/resources.htm
+    * RadBound http://www.socsci.ru.nl:8180/RaFD2/RaFD?p=register
 
     .. [TFD] Joshua Susskind, Adam Anderson, and Geoffrey Hinton,
        *The Toronto Face Database*,
@@ -34,13 +33,20 @@ class TorontoFaceDatabase(H5PYDataset):
 
     Parameters
     ----------
+    size : int
+        Either 48 or 96 which will give images of size 48x48 or 96x96.
+        Defaults to 48.
     which_set :
-
-    """  # noqa
-    filename = 'toronto_face_database.hdf5'
+        One of 'unlabeled', 'train', 'valid' or 'test'.
+    """
     default_transformers = uint8_pixels_to_floatX(('features',))
 
-    def __init__(self, which_set, **kwargs):
+    def __init__(self, which_set, size=48, **kwargs):
+        if size not in (48, 96):
+            raise ValueError("size argument must be 48 or 96")
+
+        self.filename = 'toronto_face_database{}.hdf5'.format(size)
+
         kwargs.setdefault('load_in_memory', True)
         super(TorontoFaceDatabase, self).__init__(
             self.data_path, which_set, **kwargs)
