429d3ff399eca69bbcb32c4cf85a0d4214c83548
Jorg Bornschein <jb@capsec.org>
2015-06-17 21:46:24 -0400

    Adopt to new H5PYDataset interface; Update docstring

--- fuel/datasets/toronto_face_database.py
+++ fuel/datasets/toronto_face_database.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-import os
 
-from fuel import config
 from fuel.datasets import H5PYDataset
 from fuel.transformers.defaults import uint8_pixels_to_floatX
+from fuel.utils import find_in_data_path
 
 
 class TorontoFaceDatabase(H5PYDataset):
     u"""Toronto Face Database.
 
-    The Toronto Face Dataset [TFD] contains face images from 8 different
+    The Toronto Face Database [TFD] contains face images from 8 different
     sources subsumed into a common dataset. To homogenize the images they
     where all centered and cropped using a face detector.
 
@@ -21,11 +20,17 @@ class TorontoFaceDatabase(H5PYDataset):
     * POFA     http://www.paulekman.com/product/
                 pictures-of-facial-affect-pofa/ ($175)
     * KDEF     http://www.emotionlab.se/resources/kdef
-    * CK+      http://www.pitt.edu/~emotion/ck-spread.htm
+    * CK+      http://www.consortium.ri.cmu.edu/ckagree/
     * MMI      http://www.mmifacedb.com/
     * MSFDE    http://psychophysiolab.com/msfde/terms.php
     * NimStim  http://www.macbrain.org/resources.htm
     * RadBound http://www.socsci.ru.nl:8180/RaFD2/RaFD?p=register
+
+    After obtaining the required permissions/licenses you can contact
+    Joshua Susskind <jmsusskind@gmail.com> to obtain a download URL
+    for the dataset. Please make sure you send him proof that you signed
+    at least http://www.consortium.ri.cmu.edu/ckagree/ .
+
 
     .. [TFD] Joshua Susskind, Adam Anderson, and Geoffrey Hinton,
        *The Toronto Face Database*,
@@ -41,7 +46,7 @@ class TorontoFaceDatabase(H5PYDataset):
     """
     default_transformers = uint8_pixels_to_floatX(('features',))
 
-    def __init__(self, which_set, size=48, **kwargs):
+    def __init__(self, which_sets, size=48, **kwargs):
         if size not in (48, 96):
             raise ValueError("size argument must be 48 or 96")
 
@@ -49,8 +54,5 @@ class TorontoFaceDatabase(H5PYDataset):
 
         kwargs.setdefault('load_in_memory', True)
         super(TorontoFaceDatabase, self).__init__(
-            self.data_path, which_set, **kwargs)
-
-    @property
-    def data_path(self):
-        return os.path.join(config.data_path, self.filename)
+            file_or_path=find_in_data_path(self.filename),
+            which_sets=which_sets, **kwargs)
