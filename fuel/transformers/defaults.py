37db51dcc47c322f131de9a055bd02c9cde09094
David Warde-Farley <wardefar@iro.umontreal.ca>
2015-07-29 03:30:53 -0400

    Dataset wrapper.

--- fuel/transformers/defaults.py
+++ fuel/transformers/defaults.py
@@ -1,8 +1,30 @@
 """Commonly-used default transformers."""
-from fuel.transformers import ScaleAndShift, Cast
+from fuel.transformers import ScaleAndShift, Cast, SourcewiseTransformer
+from fuel.transformers.image import ImagesFromBytes
 
 
 def uint8_pixels_to_floatX(which_sources):
     return (
         (ScaleAndShift, [1 / 255.0, 0], {'which_sources': which_sources}),
         (Cast, ['floatX'], {'which_sources': which_sources}))
+
+
+class ToBytes(SourcewiseTransformer):
+    """Transform a stream of ndarray examples to bytes.
+
+    Notes
+    -----
+    Used for retrieving variable-length byte data stored as, e.g. a uint8
+    ragged array.
+
+    """
+    def transform_source_example(self, example, _):
+        return example.tostring()
+
+    def transform_source_batch(self, batch, _):
+        return [example.tostring() for example in batch]
+
+
+def rgb_images_from_encoded_bytes(which_sources):
+    return ((ToBytes, [], {'which_sources': which_sources}),
+            (ImagesFromBytes, [], {}))
