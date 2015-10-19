5a53422c646707bc73ba45f212bb3b3826565d59
Arnaud Bergeron <abergeron@gmail.com>
2015-10-19 15:21:24 -0400

    Fix dataset loading and docs.

--- fuel/datasets/caltech101.py
+++ fuel/datasets/caltech101.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from fuel.utils import find_in_data_path
 from fuel.datasets import H5PYDataset
+from fuel.transformers.defaults import uint8_pixels_to_floatX
 
 
 class CalTech101(H5PYDataset):
@@ -9,14 +10,17 @@ class CalTech101(H5PYDataset):
     Parameters
     ----------
     which_sets : tuple of str
-        Which split to load. Valid values are 'train', 'valid' and 'test'.
+        Which split to load. Valid values are 'train' and 'test'.
 
     """
+    filename = 'caltech101.hdf5'
+    default_transformers = uint8_pixels_to_floatX(('features'),)
+
     def __init__(self, which_sets, load_in_memory=True, **kwargs):
-        self.filename = 'caltech101.hdf5'
-        super(CalTech101, self).__init__(
-            self.data_path, which_sets=which_sets,
-            load_in_memory=load_in_memory, **kwargs)
+        super(CalTech101, self).__init__(file_or_path=self.data_path,
+                                         which_sets=which_sets,
+                                         load_in_memory=load_in_memory,
+                                         **kwargs)
 
     @property
     def data_path(self):
