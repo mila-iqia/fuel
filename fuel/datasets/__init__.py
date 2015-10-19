9fe927cd495041e147c4ca5457e5255dab802596
Arnaud Bergeron <abergeron@gmail.com>
2015-10-19 14:59:24 -0400

    Add the dataset to the __init__ file.

--- fuel/datasets/__init__.py
+++ fuel/datasets/__init__.py
@@ -7,6 +7,7 @@ from fuel.datasets.adult import Adult
 from fuel.datasets.binarized_mnist import BinarizedMNIST
 from fuel.datasets.cifar10 import CIFAR10
 from fuel.datasets.cifar100 import CIFAR100
+from fuel.datasets.caltech101 import CalTech101
 from fuel.datasets.caltech101_silhouettes import CalTech101Silhouettes
 from fuel.datasets.iris import Iris
 from fuel.datasets.mnist import MNIST
