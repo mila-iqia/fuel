f138f1971d56c347a2ef27e49020586e24ec23cd
David Warde-Farley <wardefar@iro.umontreal.ca>
2015-12-02 14:20:22 -0500

    Add test ground truth.

--- .travis-data.sh
+++ .travis-data.sh
@@ -16,6 +16,7 @@ cd $FUEL_DATA_PATH
 for dataset in "$@"; do
   if [ "$dataset" == "ilsvrc2010" ]; then
     wget "http://www.image-net.org/challenges/LSVRC/2010/download/ILSVRC2010_devkit-1.0.tar.gz"
+    wget "http://www.image-net.org/challenges/LSVRC/2010/ILSVRC2010_test_ground_truth.txt"
   else
     download $dataset
   fi
