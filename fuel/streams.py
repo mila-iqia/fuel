4d8721170eaec8b5df844a0d3a3de5ce2de1e7b5
Peng Liu <laupeng1989@gmail.com>
2016-03-06 16:18:57 +0800

    recover numpy.ndarray alignment after transmisson

--- fuel/streams.py
+++ fuel/streams.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
 import zmq
+import numpy
 from six import add_metaclass, iteritems
 
 from fuel.iterator import DataIterator
@@ -231,6 +232,8 @@ class ServerDataStream(AbstractDataStream):
         if not self.connected:
             self.connect()
         data = recv_arrays(self.socket)
+        data = [numpy.require(arr, requirements='A')
+                for arr in data]
         return tuple(data)
 
     def get_epoch_iterator(self, **kwargs):
