237e3830a4d4632743ba3533905f1dfeb38134f2
Florian Bordes <florian.bordes@umontreal.ca>
2017-09-14 15:51:56 -0400

    Move camvid in alphabetical order

--- fuel/converters/__init__.py
+++ fuel/converters/__init__.py
@@ -13,10 +13,10 @@ command-line arguments, and is expected to download the required files.
 from fuel.converters import adult
 from fuel.converters import binarized_mnist
 from fuel.converters import caltech101_silhouettes
+from fuel.converters import camvid
 from fuel.converters import celeba
 from fuel.converters import cifar10
 from fuel.converters import cifar100
-from fuel.converters import camvid
 from fuel.converters import dogs_vs_cats
 from fuel.converters import iris
 from fuel.converters import mnist
@@ -30,10 +30,10 @@ all_converters = (
     ('adult', adult.fill_subparser),
     ('binarized_mnist', binarized_mnist.fill_subparser),
     ('caltech101_silhouettes', caltech101_silhouettes.fill_subparser),
+    ('camvid', camvid.fill_subparser),
     ('celeba', celeba.fill_subparser),
     ('cifar10', cifar10.fill_subparser),
     ('cifar100', cifar100.fill_subparser),
-    ('camvid', camvid.fill_subparser),
     ('dogs_vs_cats', dogs_vs_cats.fill_subparser),
     ('iris', iris.fill_subparser),
     ('mnist', mnist.fill_subparser),
